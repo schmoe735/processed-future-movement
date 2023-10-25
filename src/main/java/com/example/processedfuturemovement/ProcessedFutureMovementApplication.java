37e77846eea8f37cf90c9f369fad33c96dd3a92b
Clifford <yourpalcliffy@gmail.com>
2023-10-26 10:13:15 +1100

    First Commit

---
+++ src/main/java/com/example/processedfuturemovement/ProcessedFutureMovementApplication.java
@@ -0,0 +1,13 @@
+package com.example.processedfuturemovement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessedFutureMovementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessedFutureMovementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/processedfuturemovement/ProcessedFutureMovementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.processedfuturemovement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessedFutureMovementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
